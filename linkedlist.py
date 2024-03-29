082769db5a43ac5f0491b61e9ab8b7c5794ad100
Dubjay18 <jejeniyi7@gmail.com>
2024-03-29 22:45:00 +0100

    linked-list rebuild

--- linkedlist.py
+++ linkedlist.py
@@ -1,76 +1,117 @@
 class Node:
     data = None
     next_node = None
-    
-    def __init__(self,data):
+
+    def __init__(self, data):
         self.data = data
-        
+
     def __repr__(self):
-        return "<Node data: %s" % self.data
-    
-class linked:
- 
+        return '<Node data: %s>' % self.data
+
+
+class Linked_list:
+
     def __init__(self):
         self.head = None
-    
+
     def is_empty(self):
-        return self.head == None
-    
+        return self.head == 0
+
     def size(self):
         current = self.head
-        count= 0
-        
+        count = 0
         while current:
-            count+=1
+            count += 1
             current = current.next_node
-            
         return count
-    
-    def append(self, data):
+
+
+    def add(self, data):
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
 
-    def insert(self,data,index):
+    def search(self, key):
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
         if index == 0:
             self.add(data)
-        
+
         if index > 0:
             new = Node(data)
-            
+
             position = index
             current = self.head
-            
+
             while position > 1:
-                current = new.next_node
+                current = Node.next_node
                 position -= 1
-            
+
             prev = current
-            next = current.next_node
-            
-            
-                
-            
+            next_node = current.next_node
+
+            prev.next_node = new
+            new.next_node = next_node
+
+    def remove(self, key):
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+
+        return current
+
+    def node_at_index(self, index):
+
+        if index == 0:
+            return self.head
+        else:
+            current = self.head
+            position = 0
+
+            while position < index:
+                current = current.next_node
+                position+=1
+
+            return current
+
     def __repr__(self):
         nodes = []
         current = self.head
-        
+
         while current:
             if current is self.head:
-                nodes.append("[Head %s]" % current.data)
+                nodes.append("[head: %s]" % current.data)
             elif current.next_node is None:
                 nodes.append("[Tail: %s]" % current.data)
             else:
                 nodes.append("[%s]" % current.data)
-            
+
             current = current.next_node
+
         return '->'.join(nodes)
 
-l = linked()
-i = 0
 
-for i in range(0,9):
-    l.append(i)
-    i+=1
-    
-print(l.__repr__())
+l = Linked_list()
+l.add(10)
+l.add(21)
+
+print(l)
